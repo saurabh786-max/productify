55f10aaedccdd2e08ffc7129d4fda58a605e24f8
saurabh786-max <saurabhshah506@gmail.com>
2026-06-03 14:06:26 +0530

    revert changes to asyncHandler

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,17 +1,11 @@
-export const asyncHandler = (requestHandler) => {
-  return async (req, res, next) => {
-    try {
-      await requestHandler(req, res, next);
-    } catch (error) {
-      console.log("ERROR MESSAGE:", error.message);
-      console.log("ERROR CAUSE:", error.cause);
-      console.log("FULL ERROR:", error);
+import { request } from "express"
 
-      return res.status(500).json({
-        success: false,
-        message: error.message,
-        cause: error.cause,
-      });
+const asyncHandler = (requestHandler)=>{
+    return async (req, res,next)=>{
+       await Promise.resolve(requestHandler(req,res,next))
+        .catch((error)=>next(error))
     }
-  };
-};
+
+}
+
+export default asyncHandler;
