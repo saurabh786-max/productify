fbb93528be913711ae7c79e0d6400f441056c0ad
saurabh786-max <saurabhshah506@gmail.com>
2026-06-03 19:13:42 +0530

    fix: modified server.js for render

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
+// import "dotenv/config";
+// import app from "./app.js";
+
+// const PORT = process.env.PORT || 3000;
+
+// if (process.env.NODE_ENV !== "production") {
+//   app.listen(PORT, () => {
+//     console.log(`Server is listening on port: ${PORT}`);
+//   });
+// }
+
+// export default app;
 import "dotenv/config";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
 
 export default app;
