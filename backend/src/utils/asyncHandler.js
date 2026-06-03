dd1843969e4c73d24ec5ca89bb5e0fb407d7226c
saurabh786-max <saurabhshah506@gmail.com>
2026-06-03 13:57:18 +0530

    add postgres detailed error logging

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,11 +1,17 @@
-import { request } from "express"
+export const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      console.log("ERROR MESSAGE:", error.message);
+      console.log("ERROR CAUSE:", error.cause);
+      console.log("FULL ERROR:", error);
 
-const asyncHandler = (requestHandler)=>{
-    return async (req, res,next)=>{
-       await Promise.resolve(requestHandler(req,res,next))
-        .catch((error)=>next(error))
+      return res.status(500).json({
+        success: false,
+        message: error.message,
+        cause: error.cause,
+      });
     }
-
-}
-
-export default asyncHandler;
+  };
+};
